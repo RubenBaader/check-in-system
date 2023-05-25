48f48e78ecc8b45389856747d84ed999672c35a3
RubenBaader <rubenbaader@gmail.com>
2023-05-25 13:10:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UCL_Programmering_Eksamen
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
